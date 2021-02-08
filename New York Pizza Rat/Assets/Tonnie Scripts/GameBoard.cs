5be8fde59f7ab953d7d39b4797a95d7d55b5a3ba
Sparkeagle2000 <43283207+Sparkeagle2000@users.noreply.github.com>
2021-02-07 20:50:22 -0500

    fixed layout to be in correct units.

---
+++ New York Pizza Rat/Assets/Tonnie Scripts/GameBoard.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoard : MonoBehaviour
+{
+    private static int boardWidth=13;
+    private static int boardHeight=20;
+    public GameObject[,] board=new GameObject[boardWidth,boardHeight];
+    // Start is called before the first frame update
+    void Start()
+    {
+        Object[] objects=GameObject.FindObjectsOfType(typeof(GameObject));
+
+        foreach (GameObject o in objects)
+        {
+            Vector2 pos=o.transform.position;
+            if(o.tag!="pizzarat")
+            {
+                board [(int)pos.x,(int)pos.y]=o;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
